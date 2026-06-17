65c3cf200c7b4be07edbc01a7ac82f0a2d41e300
sada1994ar-alt <sa.da1994ar@gmail.com>
2026-06-17 18:13:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
 import os
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-translator = Translator()
 
 @bot.event
 async def on_ready():
@@ -14,13 +13,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user or message.content.startswith('!'):
-        await bot.process_commands(message)
+    if message.author == bot.user:
         return
     
-    # ترجمه ساده برای تست
-    translated = translator.translate(message.content, dest='fa')
-    await message.channel.send(f"ترجمه: {translated.text}")
+    # ترجمه با deep-translator
+    translated_text = GoogleTranslator(source='auto', target='fa').translate(message.content)
+    await message.channel.send(f"ترجمه: {translated_text}")
     await bot.process_commands(message)
 
 bot.run(os.environ['TOKEN'])
